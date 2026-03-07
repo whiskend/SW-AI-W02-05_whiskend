226087a2d6da5cdf37cc0a1a18145889387eae8f
이경근 <igyeong-geun@igyeong-geun-ui-MacBookPro.local>
2026-03-07 20:21:55 +0900

    코어타임 첫 번째 문제 풀이

---
+++ week2/basic/core01.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow = 0
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+
+        return slow + 1
