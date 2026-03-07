df734cb1c911fa450f19eeeb72c8c34cc9bd90dc
이경근 <igyeong-geun@igyeong-geun-ui-MacBookPro.local>
2026-03-07 16:02:36 +0900

    첫 번째 베이직 문제 풀이

--- week2/basic/01_python_dict.py
+++ week2/basic/01_python_dict.py
@@ -42,13 +42,18 @@ def find_above_average_students(students):
         tuple: (평균 점수, 평균 이상 학생 이름 리스트)
     """
     # TODO: 모든 학생의 점수를 리스트로 추출하세요
-    pass
+    scores = []
+    for student in students:
+        scores.append(student["score"])
     
     # TODO: 평균 점수를 계산하세요
-    pass
+    average = sum(scores) / len(scores)
     
     # TODO: 평균 이상인 학생들의 이름을 리스트로 추출하세요
-    pass
+    above_average_students = []
+    for student in students:
+        if student["score"] > average:
+            above_average_students.append(student["name"])
     
     return average, above_average_students
 
