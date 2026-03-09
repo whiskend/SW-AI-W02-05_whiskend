994bad5dff546c469be046e2227763049c38f2de
이경근 <igyeong-geun@igyeong-geun-ui-MacBookPro.local>
2026-03-09 16:23:09 +0900

    난이도하_정수론소수찾기

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,23 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+
+def find_primary(num, count):
+    for _ in range(num):
+        number = int(input())
+
+        if number == 1:
+            continue
+
+        for i in range(2, int(number**0.5) + 1):
+            if number % i == 0:
+                break
+        else:
+            count += 1
+    return count
+
+
+if __name__ == "__main__":
+    num = int(input())
+    count = 0
+
+    print(find_primary(num, count))
