4127427b73e7a34c0a05a7294b82a6b238e9b477
이경근 <igyeong-geun@igyeong-geun-ui-MacBookPro.local>
2026-03-10 15:43:08 +0900

    난이도중_완전탐색_차이를최대로_실버2.py 풀이

--- week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,2 +1,21 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
+
+from itertools import permutations
+
+def solution():
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    print(A)
+    answer = -float('inf')
+    for p in permutations(A):
+        result = 0
+        for i in range(2, N+1):
+            result += abs(p[i-2] - p[i-1])
+        answer = max(answer, result)
+
+    print(answer)
+
+if __name__ == "__main__":
+    solution()
