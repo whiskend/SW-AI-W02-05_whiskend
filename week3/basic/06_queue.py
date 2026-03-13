273d1d504956f0ca75796c194091c15b7b3953a9
이경근 <igyeong-geun@igyeong-geun-ui-MacBookPro.local>
2026-03-14 01:08:36 +0900

    06_queue.py 풀이

--- week3/basic/06_queue.py
+++ week3/basic/06_queue.py
@@ -44,7 +44,10 @@ def process_print_queue(jobs):
     # TODO: 큐가 비어있지 않은 동안 반복
     ## 큐에서 작업 꺼내기
     ## 작업 처리 (출력 및 리스트에 추가)
-    pass
+    while queue:
+        temp = queue.popleft()
+        print(temp)
+        processed.append(temp)
     
     return processed
 
