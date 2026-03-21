0b548e7ff52d7d8965fffaa332b069f6f9f25114
이경근 <lsg008@naver.com>
2026-03-21 20:06:26 +0900

    05_dfs.py 풀이

--- week4/basic/05_dfs.py
+++ week4/basic/05_dfs.py
@@ -43,14 +43,17 @@ def dfs(graph, start, visited=None):
         방문 순서 리스트
     """
     # TODO: visited가 None이면 초기화
-    pass
+    if visited is None:
+        visited = []
     
     # TODO: 현재 정점 방문
-    pass
+    visited.append(start)
     
     # TODO: 인접한 정점들에 대해 재귀
     ## 방문하지 않은 정점이면 재귀 호출
-    pass
+    for neighnor in graph[start]:
+        if neighnor not in visited:
+            dfs(graph, neighnor, visited)
     
     return visited
 
