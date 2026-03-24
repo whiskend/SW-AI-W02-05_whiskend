5ba1da5bff334e67afc75bea2a9e58f85371f47b
이경근 <lsg008@naver.com>
2026-03-25 01:28:26 +0900

    난이도중_BFS_동전2_골드5.py 풀이

--- week4/problem-solving/난이도중_BFS_동전2_골드5.py
+++ week4/problem-solving/난이도중_BFS_동전2_골드5.py
@@ -1,2 +1,26 @@
 # BFS - 동전 2 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2294
+
+from collections import deque
+
+n, k = map(int, input().split())
+coins = list(int(input()) for _ in range(n))
+
+visited = [-1] * (k + 1)
+queue = deque([0])
+visited[0] = 0
+
+while queue:
+    current = queue.popleft()
+
+    for coin in coins:
+        next_value = current + coin
+
+        if next_value > k:
+            continue
+
+        if visited[next_value] == -1:
+            visited[next_value] = visited[current] + 1
+            queue.append(next_value)
+
+print(visited[k])
