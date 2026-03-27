d95c905fc72fae554685e6e314f9c6060675aedc
이경근 <lsg008@naver.com>
2026-03-27 13:50:51 +0900

    03_greedy_coin.py 풀이

--- week5/basic/03_greedy_coin.py
+++ week5/basic/03_greedy_coin.py
@@ -46,7 +46,15 @@ def make_change_greedy(change, coins):
     # TODO: 각 동전에 대해 반복
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
-    pass
+    for coin in coins:
+        count = None # 거슬러줄 동전의 개수
+        count = change // coin
+
+        if count > 0:
+            total_coins += count
+            result[coin] = count
+        
+        change %= coin
     
     return total_coins, result
 
