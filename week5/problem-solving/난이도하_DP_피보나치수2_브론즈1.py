bcf7839fe0221611649d1c51eb854e2d8a605e5d
이경근 <lsg008@naver.com>
2026-03-27 20:31:07 +0900

    난이도하_DP_피보나치수2_브론즈1.py 풀이

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,23 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+n = int(input())
+memo = [0] * (n+1)
+
+def dp(n, memo):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+
+    result = 0
+    if memo[n] != 0:
+        result = memo[n]
+        return result
+    
+    result = dp(n-1, memo) + dp(n-2, memo)
+    memo[n] = result
+    
+    return result
+
+print(dp(n, memo))
