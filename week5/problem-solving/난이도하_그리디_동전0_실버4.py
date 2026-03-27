ebb7afa14c1581e61d021cbc7b737bcf476521e2
이경근 <lsg008@naver.com>
2026-03-27 21:02:21 +0900

    "난이도하_그리디_동전0_실버4.py" 풀이

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,13 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+
+cointypecount, total = map(int, input().split())
+cointype = [int(input().strip()) for i in range(cointypecount)]
+cointype = cointype[::-1]
+
+result = 0
+for coin in cointype:
+    result += (total // coin)
+    total %= coin
+
+print(result)
