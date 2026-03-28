a2dc7dc16bdd59ca4420c3e673f0544c4ecae1e7
이경근 <lsg008@naver.com>
2026-03-28 13:34:13 +0900

    난이도하_DP_01타일_실버3.py 풀이

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,22 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+
+N = int(input())
+
+def tile(N):
+    if N == 1:
+        return 1
+    if N == 2:
+        return 2
+    
+    n_2 = 1
+    n_1 = 2
+    for _ in range(3, N+1):
+        temp = n_1 + n_2
+        n_2 = n_1
+        n_1 = temp % 15746
+        print(f"n_1: {n_1} n_2: {n_2} for: temp: {temp}")
+
+    return n_1
+
+print(tile(N))
