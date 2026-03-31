5a544f4e26586f0bacfe7b5c739b0412218bea39
이경근 <lsg008@naver.com>
2026-03-31 15:35:41 +0900

    난이도중_DP_동전_골드5.py 풀이

---
+++ week5/core/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+dp[0][0] = triangle[0][0]
+        # dp[1][0] = triangle[1][0]
+        # dp[1][1] = triangle[1][1]
+
+        # for n in range(size):
+        #     for i in range(0, n):
+        #         dp[n][i] = min(dp[n-1][i]+triangle[n][i], dp[n-1][i-1]+triangle[n][i])
+
+        # return min(dp[n])
--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,18 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+
+testEA = int(input())
+for _ in range(testEA):
+    coinEA = int(input()) # 3
+    cointype = list(map(int, input().split())) # 1 5 
+    
+    target = int(input()) # 100
+
+    dp = [0] * (target + 1)
+    dp[0] = 1
+    for coin in cointype: #cointype : 1, 2, 5
+        for n in range(coin, target+1): # 5, 22
+            if dp[n-coin] != 0:
+                dp[n] += dp[n-coin]
+
+    print(dp[target])
