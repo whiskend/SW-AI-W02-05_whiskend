c11a6dd84a3c4a474b5e7c9db25160418bfc0dbd
이경근 <lsg008@naver.com>
2026-04-10 15:52:12 +0900

    난이도하_큐_카드2_실버4.py 풀이

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -1,2 +1,18 @@
 # 큐 - 카드2 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/2164
+
+from collections import deque
+N = int(input())
+
+# 입력 받은 숫자만큼 반복문으로 큐에 집어넣기
+q = deque()
+for i in range(1,N+1):
+    q.append(i)
+
+# 큐에서 하나 빼서 버리고, 하나 뺏다가 다시 넣기
+while(len(q) != 1):
+    q.popleft()
+    q.append(q.popleft())
+
+x = q.popleft()
+print(x)
--- week5/problem-solving/난이도중_그리디_신입사원_실버1.py
+++ week5/problem-solving/난이도중_그리디_신입사원_실버1.py
@@ -1,2 +1,12 @@
 # 그리디 - 신입 사원 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1946
+
+score = [(3, 6),(7, 3),(4, 2),(1, 4),(5, 7),(2, 5),(6, 1)]
+# print([item[0] for item in score])
+
+row0 = sorted(score, key = lambda item: item[0], reverse = True)
+row1 = sorted(score, key = lambda item: item[1], reverse = True)
+print(row0)
+print(row1)
+
+if row0[0] 
